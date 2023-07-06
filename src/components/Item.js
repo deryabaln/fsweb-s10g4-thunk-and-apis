7c5e99195c3ba72bd88ccdab1219f4ad8e4b36d9
derya <deryabalin@gmail.com>
2023-07-06 19:46:47 +0300

    proje tamamlandı

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import React from 'react';
 
 function Item({ data }) {
   return (
     <div className='shadow-md bg-white text-center'>
-      <p className='text-2xl p-10'>{data.title}</p>
+      <p className='text-2xl  bg-blue-200 rounded text-blue-800 p-10'>{data.activity}</p>
     </div>
   )
 }
